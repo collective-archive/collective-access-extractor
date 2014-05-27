c112a9f6e530ca9232fd7bd165e2e25f9688559a
Mario Signore <mario.j.signore@gmail.com>
2014-05-27 19:36:19 -0400

    Fetch all records in one fell swoop

--- spec/gateway.spec.js
+++ spec/gateway.spec.js
@@ -1,4 +1,5 @@
 Gateway = require('../lib/gateway')
+Extractor = require('../lib/extractor')
 
 describe("Gateway", function() {
   beforeEach(function() {
@@ -10,28 +11,8 @@ describe("Gateway", function() {
     this.gateway = new Gateway(this.params);
   });
 
-  it("can fetch all the objects", function(done) {
-    var test = this;
-
-    this.gateway.getModel('ca_objects', function(err, body) {
-      if(err) {
-        test.fail(err);
-        done();
-      }
-      done();
-    });
-  });
-
-  it("can fetch all the entities", function(done) {
-    var test = this;
-
-    this.gateway.getModel('ca_entities', function(err, body) {
-      if(err) {
-        test.fail(err);
-        done();
-      }
-      done();
-    });
+  it("can fetch all the records", function(done) {
+    Extractor.fetchAllRecords(this.params, function(err, id, type, data){console.log(data);}, function(){done();})
   });
 
   it("can fetch a single item", function(done) {
